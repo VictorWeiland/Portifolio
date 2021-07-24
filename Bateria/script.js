b8c0111a0973ea1b0f4cf238c62814ecbc37e738
VictorWeiland <victoranacletodealmeida13@gmail.com>
2021-07-24 20:42:36 -0300

    Subindo Parte 1 Projeto Bateria

---
+++ Bateria/script.js
@@ -0,0 +1,21 @@
+document.body.addEventListener('keyup', (event)=>{
+    playSound(event.code.toLowerCase());
+});
+
+function playSound(sound){
+  let audioElement = document.querySelector(`#s_${sound}`);
+  let KeyElement = document.querySelector(`div[data-key="${sound}"]`)
+
+  if(audioElement){
+    audioElement.currentTime = 0;
+    audioElement.play();
+  }
+
+  if(KeyElement){
+      KeyElement.classList.add('active');
+
+      setTimeout(()=>{
+        KeyElement.classList.remove('active');
+      }, 300);
+  }
+}
