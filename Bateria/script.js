1952b19c36209c055a5609745060a931f2508d63
VictorWeiland <victoranacletodealmeida13@gmail.com>
2021-07-24 21:34:21 -0300

    Projeto Bateria Concluido

--- Bateria/script.js
+++ Bateria/script.js
@@ -2,6 +2,16 @@ document.body.addEventListener('keyup', (event)=>{
     playSound(event.code.toLowerCase());
 });
 
+document.querySelector('.composer button').addEventListener('click', () =>{
+  let song = document.querySelector('#input').value;
+
+  if(song !== ''){
+    let songArray = song.split('');
+    playComposition(songArray)
+  }
+});
+
+
 function playSound(sound){
   let audioElement = document.querySelector(`#s_${sound}`);
   let KeyElement = document.querySelector(`div[data-key="${sound}"]`)
@@ -17,5 +27,16 @@ function playSound(sound){
       setTimeout(()=>{
         KeyElement.classList.remove('active');
       }, 300);
+  }
+}
+
+function playComposition(songArray){
+  let wait =0;
+
+  for(let songItem of songArray){
+    setTimeout(()=>{
+      playSound(`key${songItem}`);
+    }, wait);
+    wait += 250;
   }
 }
