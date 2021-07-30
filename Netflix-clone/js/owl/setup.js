ebdabba6afbb2e8bf5657f291c279f2a1bfbfde7
VictorWeiland <victoranacletodealmeida13@gmail.com>
2021-07-30 01:14:40 -0300

    Concluindo Clone NetFlix

---
+++ Netflix-clone/js/owl/setup.js
@@ -0,0 +1,55 @@
+$('.owl-carousel').owlCarousel({
+  loop:false,
+  margin:10,
+  nav:false,
+  responsive:{
+      0:{
+          items:2
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+function tHouseCards(){
+    window.location.href="https://www.youtube.com/watch?v=0F49tqX5sEs";
+}
+
+function tUmbrella(){
+    window.location.href="https://www.youtube.com/watch?v=5_4SW8HHfUs";
+}
+function tStarWars(){
+    window.location.href="https://www.youtube.com/watch?v=Cmgv3fwoxQw";
+}
+function tPokemon(){
+    window.location.href="https://www.youtube.com/watch?v=ikwHWX-ZWnc";
+}
+function tGuerraCivil(){
+    window.location.href="https://www.youtube.com/watch?v=FRLYZn5PGuA";
+}
+function tHobbit(){
+    window.location.href="https://www.youtube.com/watch?v=VKkFW5TTB6g";
+}
+function tBreakingBad(){
+    window.location.href="https://www.youtube.com/watch?v=FH_Pm-0kuFc";
+}
+function tDrHouse(){
+    window.location.href="https://www.youtube.com/watch?v=dVlQJO4HZj0";
+}
+function tSobrenatural(){
+    window.location.href="https://www.youtube.com/watch?v=gE4mgKBWZkE";
+}
+function tSmallville(){
+    window.location.href="https://www.youtube.com/watch?v=yBHfuph4WWY";
+}
+function tVingadores(){
+    window.location.href="https://www.youtube.com/watch?v=3BXq73F2_z4";
+}
+
+function inicio(){
+    window.location.href="index.html"
+}
