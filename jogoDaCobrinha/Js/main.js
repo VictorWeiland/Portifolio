c81524abb60bf27b3920db9d7c99ba66aa1bc1e2
VictorWeiland <victoranacletodealmeida13@gmail.com>
2021-07-31 19:39:15 -0300

    definindo movimentos da cobrinha

--- jogoDaCobrinha/Js/main.js
+++ jogoDaCobrinha/Js/main.js
@@ -1,17 +1,24 @@
+
+/*declarando variaveis do fundo*/
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+/**declarando variaveis da cobra */
 let snake = [];
 snake[0] = {
   x: 8 * box,
   y: 8 * box
 }
+/*declarando variaveis das direções*/
+let direction = "right";
 
+/**função do fundo */
 function criarBG(){
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box,16 * box);
 }
 
+/**função da cobrinha */
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
       context.fillStyle = "green";
@@ -19,5 +26,30 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+/**função do inicio do jogo */
+function iniciarJogo(){
+  criarBG();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+  /** falando as direções  */
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY+= box;
+
+      /**retira o ultimo elemento*/
+      snake.pop();
+
+        let newHead = {
+          x: snakeX,
+          y: snakeY
+        }
+
+        snake.unshift(newHead);
+    }
+
+
+let jogo = setInterval(iniciarJogo, 100);
+
