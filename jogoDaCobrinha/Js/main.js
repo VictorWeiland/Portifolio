b7e6632f734066b48c647a991b7f476d451ceac8
VictorWeiland <victoranacletodealmeida13@gmail.com>
2021-07-31 20:47:32 -0300

    difinindo aleatoriedade da fruta e aumentando tamanho da cobrinha

--- jogoDaCobrinha/Js/main.js
+++ jogoDaCobrinha/Js/main.js
@@ -66,8 +66,18 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY+= box;
 
+    /*quando comer a maça, a maçã some*/
+    if(snakeX != food.x || snakeY != food.y){
       /**retira o ultimo elemento*/
       snake.pop();
+
+    }
+    else{
+          food.x = Math.floor(Math.random() * 15 + 1) * box;
+          food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+      
+      
 
         /*colocando nova cabeça ao se movimentar*/
         let newHead = {
