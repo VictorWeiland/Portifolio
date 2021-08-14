d0e3c5ca29b1405abf6e162c26fc190fbdb71465
VictorWeiland <victoranacletodealmeida13@gmail.com>
2021-08-14 19:56:58 -0300

    Concluindo o Projeto do cursoBootstrap

--- cursoBootstrap/js/scripts.js
+++ cursoBootstrap/js/scripts.js
@@ -146,40 +146,39 @@ $( document ).ready(function() {
     }
   }
 
-  // scroll para as seções
+//scroll para seções
+let navBtn = $('.nav-item');
 
-  let navBtn = $('.nav-item');
+let bannerSection = $('#mainSlider');
+let aboutSection = $('#about-area');
+let servicesSection = $('#services-area');
+let teamSection = $('#team-area');
+let portfolioSection = $('#portfolio-area');
+let contactSection = $('#contact-area');
 
-  let bannerSection = $('#mainSlider');
-  let aboutSection = $('#about-area');
-  let servicesSection = $('#services-area');
-  let teamSection = $('#team-area');
-  let portfolioSection = $('#portfolio-area');
-  let contactSection = $('#contact-area');
+let scrollTo = '';
 
-  let scrollTo = '';
+$(navBtn).Click(function(){
 
-  $(navBtn).click(function() {
+  let btnid = $(this).attr('id'); 
 
-    let btnId = $(this).attr('id');
+  if(btnid == 'about-menu'){
+    scrollTo = aboutSection;
+  }else if (btnid == 'services-menu' ){
+    scrollTo = servicesSection;
+  }else if (btnid == 'team-menu'){
+    scrollTo = teamSection;
+  }else if (btnid == 'portfolio-menu'){
+    scrollTo = portfolioSection;
+  }else if (btnid == 'contact-menu'){
+    scrollTo = contactSection;
+  }else{
+    scrollTo = bannerSection;
+  }
 
-    if(btnId == 'about-menu') {
-      scrollTo = aboutSection;
-    } else if(btnId == 'services-menu') {
-      scrollTo = servicesSection;
-    } else if(btnId == 'team-menu') {
-      scrollTo = teamSection;
-    } else if(btnId == 'portfolio-menu') {
-      scrollTo = portfolioSection;
-    } else if(btnId == 'contact-menu') {
-      scrollTo = contactSection;
-    } else {
-      scrollTo = bannerSection;
-    }
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $(scrollTo).offset().top - 70
+  }, 1500);
 
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $(scrollTo).offset().top - 70
-    }, 1500);
   });
-
 });
